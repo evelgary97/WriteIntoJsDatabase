5c1ef004fe6dd107977a1c76bc8697c09239e95d
Evelio Vega Bermudez <73418329+evelgary97@users.noreply.github.com>
2021-08-12 00:17:10 -0400

    Add project files.

---
+++ Write Into Js Database/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Write_Into_Js_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.tbModelo = new System.Windows.Forms.TextBox();
+            this.tbImagen = new System.Windows.Forms.TextBox();
+            this.tbPrecioUsd = new System.Windows.Forms.TextBox();
+            this.tbPrecioCup = new System.Windows.Forms.TextBox();
+            this.tbPriodidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Cantidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Modelo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Imagen";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 272);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "PrecioUsd";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 17);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "PrecioCup";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(43, 361);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 17);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Prioridad";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(153, 26);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(642, 22);
+            this.tbNombre.TabIndex = 1;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(153, 71);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(642, 22);
+            this.tbCategoria.TabIndex = 2;
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(153, 108);
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(642, 22);
+            this.tbDescripcion.TabIndex = 3;
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(153, 146);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(642, 22);
+            this.tbCantidad.TabIndex = 4;
+            this.tbCantidad.Text = "1";
+            // 
+            // tbModelo
+            // 
+            this.tbModelo.Location = new System.Drawing.Point(153, 185);
+            this.tbModelo.Name = "tbModelo";
+            this.tbModelo.Size = new System.Drawing.Size(642, 22);
+            this.tbModelo.TabIndex = 5;
+            // 
+            // tbImagen
+            // 
+            this.tbImagen.Location = new System.Drawing.Point(153, 224);
+            this.tbImagen.Name = "tbImagen";
+            this.tbImagen.Size = new System.Drawing.Size(642, 22);
+            this.tbImagen.TabIndex = 6;
+            this.tbImagen.Click += new System.EventHandler(this.FileChooser);
+            // 
+            // tbPrecioUsd
+            // 
+            this.tbPrecioUsd.Location = new System.Drawing.Point(153, 267);
+            this.tbPrecioUsd.Name = "tbPrecioUsd";
+            this.tbPrecioUsd.Size = new System.Drawing.Size(642, 22);
+            this.tbPrecioUsd.TabIndex = 7;
+            this.tbPrecioUsd.Text = "0";
+            // 
+            // tbPrecioCup
+            // 
+            this.tbPrecioCup.Location = new System.Drawing.Point(153, 312);
+            this.tbPrecioCup.Name = "tbPrecioCup";
+            this.tbPrecioCup.Size = new System.Drawing.Size(642, 22);
+            this.tbPrecioCup.TabIndex = 8;
+            this.tbPrecioCup.Text = "0";
+            // 
+            // tbPriodidad
+            // 
+            this.tbPriodidad.Location = new System.Drawing.Point(153, 356);
+            this.tbPriodidad.Name = "tbPriodidad";
+            this.tbPriodidad.Size = new System.Drawing.Size(642, 22);
+            this.tbPriodidad.TabIndex = 9;
+            this.tbPriodidad.Text = "100";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(376, 404);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(98, 33);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 468);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbPriodidad);
+            this.Controls.Add(this.tbPrecioCup);
+            this.Controls.Add(this.tbPrecioUsd);
+            this.Controls.Add(this.tbImagen);
+            this.Controls.Add(this.tbModelo);
+            this.Controls.Add(this.tbCantidad);
+            this.Controls.Add(this.tbDescripcion);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Write Into Db";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.TextBox tbModelo;
+        private System.Windows.Forms.TextBox tbImagen;
+        private System.Windows.Forms.TextBox tbPrecioUsd;
+        private System.Windows.Forms.TextBox tbPrecioCup;
+        private System.Windows.Forms.TextBox tbPriodidad;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
---
+++ Write Into Js Database/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Write_Into_Js_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FileChooser(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tbImagen.Text = fileDialog.FileName;
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!tbImagen.Text.Equals(""))
+            {
+                var nombre = tbNombre.Text;
+                var categoria = tbCategoria.Text;
+                var descripcion = tbDescripcion.Text;
+                var cantidad = tbCantidad.Text;
+                var modelo = tbModelo.Text;
+                var imagen = tbImagen.Text;
+                var imagenPrint = SelectName(imagen);
+                var precioUsd = tbPrecioUsd.Text;
+                var precioCup = tbPrecioCup.Text;
+                var prioridad = tbPriodidad.Text;
+
+                var sw = new StreamWriter("database.js", true);
+                sw.WriteLine($"productos.push(new Product('{nombre}', categorias[{categoria}], '{descripcion}', {cantidad}, '{modelo}', 'static/prod-img/{imagenPrint}', {precioUsd}, {precioCup}, {prioridad}));");
+                sw.Close();
+
+                var source = new FileInfo(imagen);
+                //C:\Users\evelgary97\Documents\Untitled-1.cs
+
+                source.CopyTo($"static\\prod-img\\{imagenPrint}", true);
+
+                tbNombre.Text = "";
+                tbCategoria.Text = "";
+                tbDescripcion.Text = "";
+                tbCantidad.Text = "1";
+                tbModelo.Text = "";
+                tbImagen.Text = "";
+                tbPrecioUsd.Text = "0";
+                tbPrecioCup.Text = "0";
+                tbPriodidad.Text = "100";
+                MessageBox.Show(this, "En talla");
+
+            }
+            else {
+                MessageBox.Show(this, "Añade una imagen idiota");
+            }
+        }
+
+        private string SelectName(string s)
+        {
+            var t = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '\\')
+                {
+                    t = i;
+                }
+            }
+            return s.Substring(t + 1);
+        }
+    }
+}
